9bbb5c79fafdeeebfad13177a041d2533322ea49
Brent Vossler <brentvossler@icloud.com>
2019-01-06 23:10:49 -0800

    authentication and user signup

--- app.js
+++ app.js
@@ -5,11 +5,16 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var expressHbs = require('express-handlebars');
 var mongoose = require('mongoose');
+var session = require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash');
+
 var indexRouter = require('./routes/index');
 
 var app = express();
 
 mongoose.connect('mongodb://localhost:27017/boathero');
+require('./config/passport');
 
 // view engine setup
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
@@ -19,6 +24,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret: 'supersecret', resave: false, saveUninitialized: false}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var csrf = require('csurf');
+passport = require('passport');  
+
+var csrfProtection = csrf();
+router.use(csrfProtection);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'BoatHero' });
 });
+
+router.get('/signup', function(req, res, next) {
+	var messages = req.flash('error');
+	res.render('signup', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
+});
+
+router.post('/signup', passport.authenticate('local.signup', {
+	successRedirect: '/profile',
+	failureRedirect: '/signup',
+	failureFlash: true
+}));
+
+router.get('/profile', function(req, res, next) {
+	res.render('profile'); 
+})
 
 module.exports = router;
